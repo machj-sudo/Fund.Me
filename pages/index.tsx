8a3af25431377dea5029c79f5a5804fde35adaf7
Jonathan Mach <jonathan.mach00@gmail.com>
2023-01-07 00:22:22 -0800

    Fix coloring and add new title header

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,15 @@ export default function Home() {
     <AppShell>
       <Flex mih={50} gap="md" justify="center" align="center" direction="column" className="h-screen">
 
-        <Title className="text-blue-500 mb-5"> Hello, Friend! </Title>
+        <div className="flex justify-center space-x-2 mb-5">
+          <Title className="text-black-500 text-4xl font-medium leading-tight"> Hello,  </Title>
+          <Title className="py-1.5 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-blue-500 text-white rounded"> Friend! </Title>
+        </div>
 
         <Avatar src="https://mdbcdn.b-cdn.net/img/new/avatars/8.webp" name="Lisa Tsang" caption="Nurse"/>
 
         <Link href={"/dashboard"}>
-          <Button radius="xl" size="lg"> Go to Dashboard </Button>
+          <Button radius="xl" size="lg" className="bg-blue-500"> Go to Dashboard </Button>
         </Link>
 
       </Flex>
